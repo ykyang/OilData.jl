fa9dc08e0f38fd689f12d84837e1c0072384bce1
Yi-Kun Yang <ykyang@gmail.com>
2021-06-18 09:19:34 -0500

    Add find_prt_current_date()

--- src/OilData.jl
+++ src/OilData.jl
@@ -8,7 +8,12 @@ import Dates
 #greet() = print("Hello World!")
 
 include("parser.jl")
-export add_day, find_prt_start_date, read_rsm, find_column_name, find_schedule_end_date
+export add_day,
+       find_prt_start_date,
+       read_rsm, 
+       find_column_name, 
+       find_schedule_end_date, 
+       find_prt_current_date
 
 include("plotly.jl")
 export plotly_trace, plotly_layout
